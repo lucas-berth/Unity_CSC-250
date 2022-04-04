cd20f0d72f4942f8ae5cabc2cde6313228ade97a
Lucas <86076720+lucas-berth@users.noreply.github.com>
2022-04-04 16:14:51 -0500

    adding the scripts from class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public name;
+
+    Plyaer(name)
     {
-        
+        this.name = name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public String getName()
     {
-        
+        return this.name;
     }
 }
